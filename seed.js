f7ceb1eb97d523fd5cc22b04df1f27af9c2f1133
rccacho <rccacho@gmail.com>
2019-07-30 10:48:52 -0700

    add initial sample data

---
+++ seed.js
@@ -0,0 +1,31 @@
+// This file allows us to seed our application with data
+// simply run: `node seed.js` from the root of this project folder.
+
+var db = require("./models");
+
+var sampleUsers = [];
+
+sampleUsers.push({
+  first_name: "Foo",
+  last_name: "Bar",
+  email: "foo_bar@gap.com",
+  password: "foo_bar",
+  location: "2F",
+  brand: "Gap",
+  admin: true
+})
+
+var sampleDevices = [];
+
+sampleDevices.push({
+  name: "iphone X",
+  type: "phone",
+  width: "375px",
+  operating_system: "iOS 11",
+  image: "https://cdn.macrumors.com/article-new/2017/09/iphonexdesign.jpg",
+  borrow_date: "",
+  return_date: "",
+  available: true,
+  charged: true,
+  working: true
+})
