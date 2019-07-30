32f88b4db2cef885974f319e3c5c0fa7a7f05e37
rccacho <rccacho@gmail.com>
2019-07-30 12:37:36 -0700

    add inital /api endpoint and got it to work

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,16 +1,18 @@
 function index(req, res){
-  res.json({
-    message: "Available Endpoints for Asset Management Tool - updated 30Jul2019",
-    documentation_url: "https://github.com/rccacho/asset_management_tool",
-    base_url: "localhost:3000",
-    endpoints: [
-      {
-        method: "GET",
-        path: "/api",
-        description: "describes available endpoints"
-      }
-    ]
-  })
+  res.json(
+    {
+      message: "Available Endpoints for Asset Management Tool - updated 30Jul2019",
+      documentation_url: "https://github.com/rccacho/asset_management_tool",
+      base_url: "localhost:3000",
+      endpoints: [
+        {
+          method: "GET",
+          path: "/api",
+          description: "describes available endpoints"
+        }
+      ]
+    }
+  );
 };
 
 module.exports = {
