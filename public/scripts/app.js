7648b81bbe8c3f7262ee1c6542ab350bff5e4a0e
rccacho <rccacho@gmail.com>
2019-07-30 15:11:09 -0700

    Tried setting up user code, getting error after ajax call to /api/user

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,19 +5,34 @@
 $(document).ready(function() {
   console.log('app.js loaded!');
 
+  var users = [];
+
   $.ajax({
     method: 'GET',
     url: '/api',
     success: handleSuccess,
     error: handleError
   });
+
+  // make a GET request for all users
+  $.ajax({
+    method: 'GET',
+    url: '/api/user',
+    success: handleSuccess,
+    error: handleError
+  });
+
 })
 
 function handleSuccess(json) {
   console.log('SUCCESS');
+  users = json;
   render();
 }
 
-function handleError(e) {
+function handleError(xhr, ajaxOptions, thrownError) {
   console.log('uh oh');
+  console.log(xhr);
+  console.log(ajaxOptions);
+  console.log(thrownError);
 }
