aab5214233fc5243d340ba8dba1c65fc07dfddf8
rccacho <rccacho@gmail.com>
2019-07-29 17:35:46 -0700

    add User and Device models

--- models/index.js
+++ models/index.js
@@ -1,2 +1,10 @@
 var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/asset_management_tool" );
+
+var User = require('./user');
+var Device = require('./device');
+
+module.exports = {
+  User: User,
+  Device: Device
+};
